4be6359e8685058b77dc24c478b48a9d23640f57
임동영 <imdong207@gmail.com>
2026-07-28 21:07:02 +0900

    feat: (#1) Spring 환경 세팅 및 데이터베이스 연결

---
+++ pitch-coach-back-spring/src/test/java/com/pitchcoach/core/CoreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pitchcoach.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
